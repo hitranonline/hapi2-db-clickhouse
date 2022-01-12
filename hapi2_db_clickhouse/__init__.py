e8f7b3e65369948117cafe5b4cd516f97711f0e6
Kochanov Roman <roman@lts.iao.ru>
2022-01-12 15:03:14 +0700

    Added updaters and dispatch to __init__

--- hapi2_db_clickhouse/__init__.py
+++ hapi2_db_clickhouse/__init__.py
@@ -1,1 +1,2 @@
-from hapi2_db_clickhouse.sqlalchemy.clickhouse import init, models
+from hapi2_db_clickhouse.sqlalchemy import updaters
+from hapi2_db_clickhouse.sqlalchemy.clickhouse import init, models, dispatch
