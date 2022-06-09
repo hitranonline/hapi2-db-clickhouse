0b32bdecb858dbcb05790be0278ea54a8e0b82ee
Roman Kochanov <rkochanov85@gmail.com>
2022-06-09 11:42:30 +0700

    Added storage2chache to the package interface

--- hapi2_db_clickhouse/__init__.py
+++ hapi2_db_clickhouse/__init__.py
@@ -1,3 +1,6 @@
 from hapi2_db_clickhouse.db.sqlalchemy import updaters
 from hapi2_db_clickhouse.db.sqlalchemy.clickhouse import init, models
 from hapi2_db_clickhouse.format import dispatch
+
+from hapi2.db.sqlalchemy.legacy import storage2cache
+
